14a68729278452407ba8a07bb94c5d3fa641f285
DualNeXoN <samuelsmerek69@gmail.com>
2023-05-09 12:10:10 +0200

    Added linux support

--- rfidhandler/src/main/java/rfidhandler/utils/PortDetector.java
+++ rfidhandler/src/main/java/rfidhandler/utils/PortDetector.java
@@ -4,12 +4,13 @@ import com.fazecast.jSerialComm.SerialPort;
 
 public abstract class PortDetector {
 	
-	public static final String DEVICE_NAME = "USB-SERIAL CH340";
+	public static final String DEVICE_NAME_WIN = "USB-SERIAL CH340";
+	public static final String DEVICE_NAME_LINUX = "USB-to-Serial Port (ch341-uart)";
 	
 	public static final String detectArduinoPort() {
 		SerialPort[] ports = SerialPort.getCommPorts();
         for(SerialPort port : ports) {
-            if(port.getDescriptivePortName().startsWith(DEVICE_NAME)) {
+            if(port.getDescriptivePortName().startsWith(DEVICE_NAME_WIN) || port.getDescriptivePortName().startsWith(DEVICE_NAME_LINUX)) {
             	return port.getSystemPortName();
             }
         }
